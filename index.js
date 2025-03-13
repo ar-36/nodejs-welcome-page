286080ebe9fd2bb35367a3a3e27ce574b2fd45a3
ar-36 <ammarzlve@gmail.com>
2025-03-14 03:01:07 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  console.log(process.versions);
+  res.render('welcome', {
+    version: process.versions.node,
+    os: process.platform
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}`);
+});
